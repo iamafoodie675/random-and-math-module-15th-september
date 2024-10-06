43ba400747873b76a710d8e418da32ec9d745f35
nabila ahmed <sumayaad2020@gmail.com>
2024-10-06 17:40:54 +0600

    abc

---
+++ trigonometric_value.py
@@ -0,0 +1,11 @@
+import math
+x =int(input("Enter a number:"))
+
+print("sin:", math.sin(x))
+print("cos:", math.cos(x))
+print("tan:", math.tan(x))
+
+print("ceil:", math.ceil(x))
+print("floor:", math.floor(x))
+print("factorial:", math.factorial(x))
+
